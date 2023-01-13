e3419e56761d56f536e837e40ec85fadfd276935
Joseph I Jess <12313215+Joseph-I-Jess@users.noreply.github.com>
2023-01-13 15:21:05 -0800

    Chose local file changes over remote.

--- hello.cpp
+++ hello.cpp
@@ -1,11 +1,7 @@
 #include <iostream>
 
 int main(void) {
-<<<<<<< HEAD
     std::cout << "Hello world, is anyone out there, stuff?";
-=======
-    std::cout << "Hello world, is anyone out thereblah blah blah online change?";
->>>>>>> bdaaba4b8814dd608e64450e99d51d5f19149fe7
 
     return 0;
 }
